c86d4b4c5022ad4d45e519e55b6505de51b24a37
AltynbekDinmukhamed <a.dimash.a_03@mail.ru>
2023-04-30 13:19:09 +0600

    Added extension and title label to watchController

--- StockApp/Controllers/WatchListViewController.swift
+++ StockApp/Controllers/WatchListViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class WatchListViewController: UIViewController {
-
+    //MARK: -LifeCycle-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setUpSearchController()
+        setUpTitleView()
+    }
+    //MARK: -Functions-
+    private func setUpSearchController() {
+        let result = SearchResultsViewController()
+        let searchVC  = UISearchController(searchResultsController: result)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
     }
 
-
+    private func setUpTitleView(){
+        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: navigationController?.navigationBar.height ?? 100))
+        
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: titleView.width - 20, height: titleView.height))
+        label.text = "Stocks"
+        label.font = .systemFont(ofSize: 40, weight: .medium)
+        navigationItem.titleView = titleView
+    }
 }
-
+//MARK: -Extension-
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let resultVC = searchController.searchResultsUpdater as? SearchResultsViewController,
+              !query.trimmingCharacters(in: CharacterSet.whitespaces).isEmpty else {
+            return
+        }
+        //optimize to reduce number of search when user stops typing
+        
+        //Call API to search
+        print(query)
+    }
+    
+}
